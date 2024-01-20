7c450e9bee00a65f32f3cd72b475fdd4f3ea08a8
PedroMendes414 <pedrooliverio4@gmail.com>
2024-01-20 18:03:21 -0300

    alterando testes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -25,52 +25,7 @@ test('Login com sucesso @login', async ({ page }) => {
   await page.getByLabel('Login realizado').click();
   await page.getByRole('button', { name: 'OK' }).click();
 
-});
+})
 
-test('Login com sucesso 1 @login', async ({ page }) => {
-
-  const texto = await page.waitForSelector('text=NEWSLETTER')
-  await texto.scrollIntoViewIfNeeded();
-
-  // navegando pelo login
-  await page.getByRole('link', { name: ' Login' }).click();
-  await page.locator('#user').click();
-  await page.locator('#user').fill('teste@gmail.com');
-  await page.locator('#password').click();
-  await page.locator('#password').fill('123456');
-  await page.locator('#password').screenshot({ path: 'screenshots/elementosenha.png' });
-  await page.screenshot({ path: 'screenshots/screenshot.png' });
-  await page.getByLabel('Lembrar de mim').check();
-  await page.getByRole('button', { name: 'login' }).click();
-  await page.getByLabel('Login realizado').click();
-  await page.getByRole('button', { name: 'OK' }).click();
-
-});
-
-test('Login com sucesso 2 @login', async ({ page }) => {
-
-
-  const texto = await page.waitForSelector('text=NEWSLETTER')
-  await texto.scrollIntoViewIfNeeded();
-
-  // navegando pelo login
-  await page.getByRole('link', { name: ' Login' }).click();
-  await page.locator('#user').click();
-  await page.locator('#user').fill('teste@gmail.com');
-  await page.locator('#password').click();
-  await page.locator('#password').fill('123456');
-  await page.locator('#password').screenshot({ path: 'screenshots/elementosenha.png' });
-  await page.screenshot({ path: 'screenshots/screenshot.png' });
-  await page.getByLabel('Lembrar de mim').check();
-  await page.getByRole('button', { name: 'login' }).click();
-  await page.getByLabel('Login realizado').click();
-  await page.getByRole('button', { name: 'OK' }).click();
-
-});
-
-// código utilizado para realizar uma ação após a execução do teste, por exemplo: um exclusão de usuário após o teste, etc...
-test.afterEach(async ({page})=> {
-  
-});
 
 
